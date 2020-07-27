8eb3a8e9e63e22c6828b3e7d31c3c9a5eb3d2ce7
DanielAzevedoM <danielazevedom@hotmail.com>
2020-07-27 16:34:24 -0300

    Aulas 1 e 2 concluídas

--- Fase 2/Modulo 2/7 - Video dinâmico/scripts.js
+++ Fase 2/Modulo 2/7 - Video dinâmico/scripts.js
@@ -1,5 +1,4 @@
 const modalOverlay = document.querySelector('.modal-overlay');
-
 const cards = document.querySelectorAll('.card');
 
 
@@ -7,11 +6,15 @@ for (let i = 0; i < cards.length; i++) {
     const card = cards[i];
 
     card.addEventListener("click", function() {
+        const videoId = card.getAttribute("id")
+
         modalOverlay.classList.add('active')
+        modalOverlay.querySelector("iframe").src = `https://youtube.com/embed/${videoId}`
         
     })
 }
 
 document.querySelector('.close-modal').addEventListener("click", function(){
-    modalOverlay.classList.remove('active')   
+    modalOverlay.classList.remove('active')
+    modalOverlay.querySelector("iframe").src = ""   
 })
