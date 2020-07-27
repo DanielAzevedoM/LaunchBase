5885d59ea0aa0b654a92d81be2e266ca2a805781
DanielAzevedoM <danielazevedom@hotmail.com>
2020-07-27 14:27:02 -0300

    Aulas 1, 2 e 3 concluídas

---
+++ Fase 2/Modulo 2/5- Modal/scripts.js
@@ -0,0 +1,17 @@
+const modalOverlay = document.querySelector('.modal-overlay');
+
+const cards = document.querySelectorAll('.card');
+
+
+for (let i = 0; i < cards.length; i++) {
+    const card = cards[i];
+
+    card.addEventListener("click", function() {
+        modalOverlay.classList.add('active')
+        
+    })
+}
+
+document.querySelector('.close-modal').addEventListener("click", function(){
+    modalOverlay.classList.remove('active')   
+})
