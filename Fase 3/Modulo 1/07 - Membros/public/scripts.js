24eb34fd988ebc3e40f12093dd2c2369f57a4343
DanielAzevedoM <danielazevedom@hotmail.com>
2020-11-13 08:56:40 -0300

    Aulas 1 e 2 finalizadas

---
+++ Fase 3/Modulo 1/07 - Membros/public/scripts.js
@@ -0,0 +1,12 @@
+const currentPage = location.pathname
+
+const menuItems = document.querySelectorAll("header .links a")
+
+for (let i = 0; i < menuItems.length; i++) {
+    const item = menuItems[i];
+    
+
+    if(currentPage.includes(item.getAttribute("href"))){
+        item.classList.add("active")
+    }
+}
